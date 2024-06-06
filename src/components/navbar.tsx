911ada3c742b4a9073417f04def9e6cd46f24faf
Nicholas Costa <nicholascostadev@gmail.com>
2024-06-05 23:22:36 -0300

    style: fix responsiveness issue on editor and navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -22,7 +22,7 @@ export function Navbar() {
             App
           </Link>
         </div>
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-4 sm:gap-8">
           {!isPending ? (
             user ? (
               <>
